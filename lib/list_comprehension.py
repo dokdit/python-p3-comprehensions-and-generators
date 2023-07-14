2e2a708c2378255dfc85e0c26343c2dec3bd5d9d
Akuei <akueidok911@gmail.com>
2023-07-14 16:26:07 -0400

    Lab completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    new_list = [elem for elem in num_list if elem%2 == 0]
+    return new_list
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = [elem +'!' for elem in sentence_list if len(elem) != 0]
+    return new_list
